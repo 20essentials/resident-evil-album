65d012de2861df317efcb6762d9440aae39277ac
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:17:21 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RESIDENT EVIL';
+export let nameSongs = [
+	"MAIN THEME", /*1*/
+	"SAVE ROOM", /*2*/
+	"THE SECOND MALFORMATION", /*3*/
+	"ALICE'S THEME", /*4*/
+	"THE OUTSIDER", /*5*/
+	"FLYING THROUGH THE AIR", /*6*/
+	"TOMANDANDY", /*7*/
+	"AXEMEN", /*8*/
+	"HEXES", /*9*/
+	"TOKYO REVISITED", /*10*/
+	"CORRIDOR", /*11*/
+	"PLANTING", /*12*/
+	"PHANTOM CHASE", /*13*/
+	"ZOMBIES UNDER ICE", /*14*/
+	"ICE PACK", /*15*/
+	"FALL BACK", /*16*/
+	"IT'S HELP", /*17*/
+	"END OF THE WORLD", /*18*/
+	"ROOFTOP", /*19*/
+	"WESKER", /*20*/
+]
